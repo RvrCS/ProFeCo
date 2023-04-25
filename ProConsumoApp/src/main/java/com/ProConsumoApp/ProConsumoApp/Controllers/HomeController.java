aa387162a726c491ed72cf01ace787e6b299fcbc
River <75063620+RvrCS@users.noreply.github.com>
2023-04-25 02:21:43 -0700

    login

---
+++ ProConsumoApp/src/main/java/com/ProConsumoApp/ProConsumoApp/Controllers/HomeController.java
@@ -0,0 +1,45 @@
+package com.ProConsumoApp.ProConsumoApp.Controllers;
+
+
+import com.ProConsumoApp.ProConsumoApp.Models.User;
+import com.ProConsumoApp.ProConsumoApp.Repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("")
+    public String index(){
+
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String crear(User user){
+        userRepo.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model){
+        List<User> users = userRepo.findAll();
+        System.out.println(users);
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
---
+++ ProConsumoApp/src/main/java/com/ProConsumoApp/ProConsumoApp/Models/User.java
@@ -0,0 +1,21 @@
+package com.ProConsumoApp.ProConsumoApp.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private String email;
+
+    private String password;
+}
