414da93ab05187d8c99c535f609e84da05083850
River <75063620+RvrCS@users.noreply.github.com>
2023-04-25 01:43:56 -0700

    ProConsumo Projecto

---
+++ ProConsumoApp/src/main/java/com/ProConsumoApp/ProConsumoApp/ProConsumoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ProConsumoApp.ProConsumoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProConsumoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProConsumoAppApplication.class, args);
+	}
+
+}
---
+++ ProConsumoApp/src/test/java/com/ProConsumoApp/ProConsumoApp/ProConsumoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProConsumoApp.ProConsumoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProConsumoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
