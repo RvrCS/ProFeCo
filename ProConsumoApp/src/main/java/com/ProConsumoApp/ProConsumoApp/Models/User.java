2cacba7e83398b16549d3e85942c908490c714f1
River <75063620+RvrCS@users.noreply.github.com>
2023-04-25 11:39:11 -0700

    API-gateway Project

--- ProConsumoApp/src/main/java/com/ProConsumoApp/ProConsumoApp/Models/User.java
+++ ProConsumoApp/src/main/java/com/ProConsumoApp/ProConsumoApp/Models/User.java
@@ -13,11 +13,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "nombre", nullable = false)
     private String nombre;
 
+    @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
     private Role role;
 }
