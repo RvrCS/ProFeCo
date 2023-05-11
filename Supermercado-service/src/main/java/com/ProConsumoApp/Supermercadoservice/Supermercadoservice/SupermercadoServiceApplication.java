6b36b953e25e3a1fbc0df7e70415a877e6c80cf8
River <75063620+RvrCS@users.noreply.github.com>
2023-05-11 15:25:26 -0700

    Eureka deleted

--- Supermercado-service/src/main/java/com/ProConsumoApp/Supermercadoservice/Supermercadoservice/SupermercadoServiceApplication.java
+++ Supermercado-service/src/main/java/com/ProConsumoApp/Supermercadoservice/Supermercadoservice/SupermercadoServiceApplication.java
@@ -2,10 +2,8 @@ package com.ProConsumoApp.Supermercadoservice.Supermercadoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SupermercadoServiceApplication {
 
 	public static void main(String[] args) {
